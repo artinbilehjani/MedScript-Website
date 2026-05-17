31f55633088fa1f7993873105adbb724e3dcff33
artinbilehjani <artinbilehjani@gmail.com>
2026-05-17 13:27:13 -0700

    added static and media

--- core/core/urls.py
+++ core/core/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
 ]
+
+# serving static nad media for development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
